ab56c75bc0ace1cf44f8d600cecf1894ec1379e8
Season Saw <season.saw@paywatchglobal.com>
2024-12-19 17:39:18 +0800

    Fix posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import App from "./App.tsx";
 import "./index.css";
 
 // Initialize PostHog
-posthog.init(
-    import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY as string,
-    {
-        api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-        loaded: (posthog) => {
-            if (import.meta.env.REACT_APP_DEV) posthog.debug();
-        }
-    }
-);
+posthog.init(import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY as string, {
+    api_host:
+        import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST ||
+        "https://us.i.posthog.com",
+});
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
